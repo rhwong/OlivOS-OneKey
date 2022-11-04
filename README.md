722f995c17cfb133971897c6b5ad230534217da6
贰狐RH <58497504+rhwong@users.noreply.github.com>
2022-11-04 10:08:25 +0800

    首次提交

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
+<div align="center">
+
 # OlivOS-OneKey
-在Linux发行版上一键源码部署OlivOS
+
+用于部署 [OlivOS](https://github.com/OlivOS-Team/OlivOS) 异步机器人框架的Linux快速部署脚本<br>
+
+注意：本项目仅用于快速部署 [OlivOS](https://github.com/OlivOS-Team/OlivOS) 本体，后续对接何种前端(如[Go-cqhtp](https://github.com/Mrs4s/go-cqhttp/))取决您自己的选择，这些部分并不包含在部署范围内，请参考[官方论坛](https://forum.olivos.run/)。
+
+注意：本脚本仅通过 Ubuntu x86_64 验证，其他发行版及类型系统如果出现问题请提交issue！
+
+</div>
+<!-- projectInfo end -->
+
+## 快速启动
+
+### 安装
+
+```shell
+wget -N https://github.com/rhwong/OlivOS-OneKey/raw/main/install_OlivOS.sh
+chmod -R 755 install_OlivOS.sh
+./install_OlivOS.sh
+```
+#### 【注意事项】
+
+1.在Conda安装时需要键入一些确认指令，当屏幕上提示按 `Enter` 时请按下，当提示是否确认时，请输入 `yes` 。许可部分可以按 `Q` 跳过阅读。
+
+请不要自行修改安装步骤中的安装位置，直接按下 `Enter` 安装到默认位置即可。
+
+2.由于conda在加入环境变量后，脚本会退出运行。此时必须重新连接到终端以生效，此时请务必断开ssh，重新连接终端。
+
+重新连接后提示符前面出现 `(base)` ，例如 `(base) root@ecs:~# ` 这样的显示就是conda成功安装了。
+
+在成功安装conda之后，我们需要重新运行一次脚本：
+
+键入 `conda activate OlivOS` 以激活OlivOS环境，然后键入 `./install_OlivOS.sh` 重新运行脚本以继续安装。
+
+### 启动
+
+依次输入如下指令来启动OlivOS，此后也如此。
+
+```shell
+conda activate OlivOS
+cd $HOME/OlivOS
+python3 main.py
+```
+
+### 更新和修复
+
+重复安装步骤，按照原本的安装方式选择即可。检测到已存在OlivOS目录时，脚本会自动拉取代码并更新依赖。
---
+++ install_OlivOS.sh
@@ -0,0 +1,405 @@
+#!/bin/bash
+
+# =================================================
+#	Description: OlivOS-OneKey
+#	Version: 1.0.3
+#	Author: RHWong
+# =================================================
+
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[信息]${Font_color_suffix}"
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Warrning="${Red_font_prefix}[警告]${Font_color_suffix}"
+Tip="${Green_font_prefix}[提示]${Font_color_suffix}"
+ret_code=`curl -o /dev/null --connect-timeout 3 -s -w %{http_code} https://google.com`
+conda_path=$HOME/miniconda3
+
+function waiting()
+{
+    i=0
+    while [ $i -le 100 ]
+    do
+    for j in '\\' '|' '/' '-'
+    do
+    printf "\t%c%c%c%c%c ${Info} 少女祈祷中... %c%c%c%c%c\r" \
+    "$j" "$j" "$j" "$j" "$j" "$j" "$j" "$j" "$j" "$j"
+    sleep 0.1
+    done
+    let i=i+4
+    done
+}
+
+# 检测本机是否为CentOS
+check_sys(){
+    if [[ -f /etc/redhat-release ]]; then
+        release="centos"
+    elif cat /etc/issue | grep -q -E -i "Debian"; then
+        release="debian"
+    elif cat /etc/issue | grep -q -E -i "Ubuntu"; then
+        release="Ubuntu"
+    elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+    elif cat /proc/version | grep -q -E -i "Debian"; then
+        release="Debian"
+    elif cat /proc/version | grep -q -E -i "Ubuntu"; then
+        release="Ubuntu"
+    elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+    # 如果是未知系统版本则输出unknown
+    else
+        release="unknown"
+    fi
+    bit=`uname -m`
+}
+
+# 如果是CentOS则返回警告
+anti_CentOS(){
+    if [[ ${release} != "centos" ]]; then
+        echo -e "${Info} 检测到当前发行版系统为 ${Green_font_prefix}[${release}]${Font_color_suffix}..."
+    else
+        echo -e "${Warrning} OlivOS官方不推荐使用${Red_font_prefix}[${release}]${Font_color_suffix}进行部署，推荐您使用Docker部署!"
+        echo -e "${Warrning} 本脚本可以运行在${Red_font_prefix}[${release}]${Font_color_suffix}上，但未经验证，可能会出现未知错误。"
+        # 继续运行请按Y
+            read -p "是否继续运行？[Y/n]:" yn
+            if [[ $yn == [Yy] ]]; then
+            echo -e "${Info} 继续运行..."
+            else
+            exit 1
+            fi
+    fi
+
+}
+
+# 如果不是x86_64则返回警告
+anti_bit(){
+    if [[ ${bit} == "x86_64" ]]; then
+        echo -e "${Info} 系统类型为 ${Green_font_prefix}[${bit}]${Font_color_suffix}，开始安装..."
+    else
+        echo -e "${Warrning} OlivOS官方不推荐使用${Red_font_prefix}[${bit}]${Font_color_suffix}进行部署!"
+        echo -e "${Warrning} 本脚本可以运行在${Red_font_prefix}[${bit}]${Font_color_suffix}上，但未经验证，可能会出现未知错误。"
+        # 继续运行请按Y
+            read -p "是否继续运行？[Y/n]:" yn
+            if [[ $yn == [Yy] ]]; then
+            echo -e "${Info} 继续运行..."
+            else
+            exit 1
+            fi
+    fi
+
+}
+
+# 判断${release}使用不同方式安装wget和git
+install_wget_git(){
+    if [[ ${release} == "centos" ]]; then
+        yum install -y wget git
+    elif [[ ${release} == "debian" || ${release} == "Ubuntu" ]]; then
+        apt-get update
+        apt-get install -y wget git
+    elif [[ ${release} == "unknown" ]]; then
+        echo -e "${Error} 未知系统版本，若无法继续运行请自行安装wget和git"
+        sleep 3
+    fi
+}
+
+# 检测是否存在conda
+check_conda(){
+    # conda命令可用，跳过安装
+    if [ -x "$(command -v conda)" ]; then
+        echo -e "${Info} 检测到已存在conda，跳过安装"
+        sleep 2
+    else
+        echo -e "${Info} 检测到未安装conda，开始安装"
+        # 删除conda目录
+        if [ -d "$conda_path" ]; then
+            echo -e "${Info} 检测到已存在conda目录，正在删除..."
+            rm -rf $conda_path
+        fi
+        echo -e "${Warrning} 注意，在安装中可能会有提示需要你点击enter键或输入yes，请按照屏幕上的提示输入！"
+        sleep 2
+        # 按下enter继续
+        read -p "按下enter键继续..."
+    # conda安装
+    # 判断系统是否为Ubuntu
+         if [ -x "$(command -v apt)" ]; then
+        echo -e "${Tip} 正在尝试安装conda..."
+        sleep 2
+        apt install -y wget
+        # 下载安装包
+            if [[ ${bit} == "x86_64" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+            elif [[ ${bit} == "aarch64" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-aarch64.sh -O miniconda.sh
+            elif [[ ${bit} == "armv7l" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-armv7l.sh -O miniconda.sh
+            elif [[ ${bit} == "i686" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86.sh -O miniconda.sh
+            elif [[ ${bit} == "ppc64le" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-ppc64le.sh -O miniconda.sh
+            elif [[ ${bit} == "s390x" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-s390x.sh -O miniconda.sh
+            elif [[ ${bit} == "i386" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86.sh -O miniconda.sh
+            else
+                echo -e "${Error} 本脚本不支持${Red_font_prefix}[${bit}]${Font_color_suffix}系统！"
+                exit 1
+            fi
+        bash miniconda.sh
+        echo -e "${Info} conda安装结束！"
+        else
+    # 使用yum安装conda
+        echo -e "${Tip} 正在尝试安装conda..."
+        yum install -y wget
+    # 下载安装包
+            if [[ ${bit} == "x86_64" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+            elif [[ ${bit} == "aarch64" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-aarch64.sh -O miniconda.sh
+            elif [[ ${bit} == "armv7l" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-armv7l.sh -O miniconda.sh
+            elif [[ ${bit} == "i686" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86.sh -O miniconda.sh
+            elif [[ ${bit} == "ppc64le" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-ppc64le.sh -O miniconda.sh
+            elif [[ ${bit} == "s390x" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-s390x.sh -O miniconda.sh
+            elif [[ ${bit} == "i386" ]]; then
+                wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86.sh -O miniconda.sh
+            else
+                echo -e "${Error} 本脚本不支持${Red_font_prefix}[${bit}]${Font_color_suffix}系统！"
+                exit 1
+            fi
+        bash miniconda.sh
+        echo -e "${Info} conda安装结束！"
+        sleep 2
+        fi
+    add_conda_path
+    source /etc/profile
+    check_conda_install
+    fi
+}
+
+# 检测conda安装是否成功
+check_conda_install(){
+    if [ -x "$(command -v conda)" ]; then
+        echo -e "${Info} conda安装成功！"
+        sleep 2
+    else
+        echo -e "${Error} conda安装失败，请手动安装conda"
+        exit 1
+    fi
+}
+
+# 将conda加入环境变量
+add_conda_path(){
+    echo -e "${Tip} 正在将conda加入环境变量..."
+    echo "export PATH=$conda_path/bin:$PATH" >> /etc/profile
+    source /etc/profile
+    echo -e "${Info} conda加入环境变量完成！"
+}
+
+# 检测本地python版本
+check_python()
+{
+    local python_version=`python3 -V 2>&1 | awk '{print $2}'`
+    # 如果版本大于3.7，小于3.8
+    if [ "$python_version" \> "3.7.0" ] && [ "$python_version" \< "3.8.99" ]; then
+        echo -e "${Info} 本地python版本为$python_version，符合要求！"
+        sleep 2
+    else
+    # 如果版本小于3.7，大于3.9
+        if [ "$python_version" \< "3.6.99" ] || [ "$python_version" \> "3.9.0" ]; then
+            echo -e "${Error} 本地python版本为$python_version，不符合要求！"
+            echo -e "${Tip} 请安装python3.7或python3.8！"
+            exit 1
+        else
+            # 尝试安装python
+
+            # 判断系统是否为Ubuntu
+             if [ -x "$(command -v apt)" ]; then
+                echo -e "${Tip} 正在尝试安装python3.8..."
+                apt install -y python3.8
+            else
+            # 使用yum安装python3.8
+                echo -e "${Tip} 正在尝试使用yum安装python3.8..."
+                yum install -y python3.8
+            fi
+            echo -e "${Info} python3.8安装完成！"
+            sleep 2
+            
+        fi
+
+    fi
+}
+
+# 检测是否安装pip
+check_pip()
+{
+    if [ -x "$(command -v pip3)" ]; then
+        # 升级pip
+         python3 -m pip install --upgrade pip
+         echo -e "${Info} pip已更新！"
+         sleep 2
+    else
+        echo -e "${Error} pip未安装！"
+        # 尝试安装pip 
+        echo -e "${Tip} 正在尝试安装pip..."
+        sleep 2
+            # 判断系统是否为Ubuntu
+             if [ -x "$(command -v apt)" ]; then
+                apt install -y python3-pip
+                echo -e "${Info} pip安装完成！"
+            else
+            # 使用yum安装pip
+                yum install -y python3-pip
+                echo -e "${Info} pip安装完成！"
+            fi
+
+        if [ -x "$(command -v pip3)" ]; then
+            echo -e "${Info} pip安装成功！"
+            sleep 2
+        else
+            echo -e "${Error} pip安装失败，请自行安装！"
+            exit 1
+        fi
+    fi
+}
+
+# 安装OlivOS
+install_OlivOS()
+{
+    cd $HOME
+    # 判断OlivOS目录下main.py是否存在  
+if [ ! -f "OlivOS/main.py" ]; then
+    echo -e "${Info} OlivOS主文件不存在，开始安装..."
+    sleep 2
+    # 如果ret_code变量值是200
+    if [ $ret_code -eq 200]; then
+        echo -e "${Info} 网络连接正常，开始下载OlivOS..."
+        git clone https://github.com/OlivOS-Team/OlivOS.git & waiting
+    else 
+    # 如果ret_code变量值不是200，使用镜像下载
+        echo -e "${Info} 网络连接异常，开始使用镜像下载OlivOS..."
+        git clone https://ghproxy.com/https://github.com/OlivOS-Team/OlivOS.git & waiting
+    fi
+    echo -e "${Tip} OlivOS下载完成！"
+    sleep 2
+else
+    echo -e "${Info} OlivOS文件已存在，无需安装！"
+    sleep 2
+    # 更新OlivOS
+    echo -e "${Tip} 正在更新OlivOS..."
+    sleep 2
+    git pull & waiting
+    echo -e "${Tip} OlivOS更新完成！"
+    sleep 2
+fi
+}
+
+# 安装或修复依赖
+install_dependence()
+{
+    echo -e "${Info} 开始安装或修复依赖..."
+    sleep 2
+    cd $HOME/OlivOS
+    if [ $ret_code -eq 200 ]; then
+        echo -e "${Info} 网络连通性良好，使用默认镜像下载"
+        pip3 install --upgrade pip
+        pip install pyyaml~=6.0 --ignore-installed
+        pip3 install --upgrade pip -r requirements.txt
+    else
+        echo -e "${Info} 网络连通性不佳，使用腾讯镜像下载"
+        pip3 install --upgrade pip -i https://mirrors.cloud.tencent.com/pypi/simple/
+        pip install pyyaml~=6.0 --ignore-installed -i https://mirrors.cloud.tencent.com/pypi/simple
+        pip3 install --upgrade pip -r requirements.txt -i https://mirrors.cloud.tencent.com/pypi/simple
+    fi
+    echo -e "${Tip} 依赖安装或修复完成！"
+    sleep 2
+}
+
+StartOlivOS()
+{
+# 启动OlivOS
+    echo -e "${Tip} 开始安装OlivOS..." 
+    check_sys
+    anti_bit
+    select_install
+}
+
+# 本地安装  
+install_local(){
+    install_wget_git
+    check_python
+    check_pip
+    install_OlivOS
+    chmod -R 766 $HOME/OlivOS
+    cd $HOME/OlivOS
+    install_dependence
+    echo -e "${Tip} OlivOS安装完成！"
+    sleep 2
+    echo -e "${Tip} 开始尝试运行，如有问题请提交issue"
+    python3 main.py
+    # 打印安装位置
+    echo -e "${Tip} OlivOS安装位置：$HOME/OlivOS"
+    # 打印OlivOS启动指令
+    echo -e "启动指令如下："
+    echo -e "${Green_font_prefix}cd $HOME/OlivOS && python3 main.py${Font_color_suffix}"
+}
+
+# conda安装
+install_conda(){
+    install_wget_git
+    check_conda
+    # 判断OlivOS环境是否已经存在
+    if [ -d "$conda_path/envs/OlivOS" ]; then
+        echo -e "${Info} OlivOS环境已存在，跳过部署！"
+    else
+        echo -e "${Info} OlivOS环境不存在，开始部署..."
+        conda init bash
+        conda create -n OlivOS python=3.8
+        conda activate OlivOS
+        echo -e "${Info} OlivOS环境部署完成！请重新连接到终端，使用${Green_font_prefix}conda activate OlivOS${Font_color_suffix}指令来激活OlivOS环境"
+        exit 1
+    fi
+    echo -e "${Tip} 正在安装OlivOS..."
+    sleep 2
+    install_OlivOS
+    chmod -R 766 $HOME/OlivOS
+    cd $HOME/OlivOS
+    install_dependence
+    echo -e "${Tip} OlivOS安装完成！"
+    sleep 2
+    echo -e "${Tip} 开始尝试运行，如有问题请提交issue"
+    python3 main.py
+    # 打印安装位置
+    echo -e "${Tip} OlivOS安装位置：$HOME/OlivOS"
+#    打印OlivOS启动指令
+    echo -e "启动指令如下："
+    echo -e "${Green_font_prefix}cd $HOME/OlivOS && conda activate OlivOS && python3 main.py${Font_color_suffix}"
+}
+
+
+# 提示选择在本地安装还是在conda安装
+select_install(){
+    echo -e "${Info} 请选择安装方式"
+    echo -e "1. 本地安装"
+    echo -e "2. conda虚拟环境安装(推荐)"
+    read -p "请输入数字:" num
+    case "$num" in
+        1)
+        install_local
+        ;;
+        2)
+        install_conda
+        ;;
+        *)
+        echo -e "${Error} 请输入正确的数字"
+        exit 1
+        ;;
+    esac
+}
+
+
+StartOlivOS
+
+
+
